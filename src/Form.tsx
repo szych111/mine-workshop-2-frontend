71c88a780f5a6ca0c92762e335b2c71000551083
Alex <szychlinska@gmail.com>
2022-03-06 19:32:27 +0100

    OpenWeather fetch with https and works!

--- src/Form.tsx
+++ src/Form.tsx
@@ -16,33 +16,36 @@ export const Form: FC = () => {
 
     const OPEN_WEATHER_KEY = "16ce5520bd032264b1ac7375550e162d"
 
-    const FETCH_OPEN_WEATHER = () => {
-        fetch(`api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${OPEN_WEATHER_KEY}`)
-        .then(response => {return response.json()})
-        .then(data => console.log(data.main))
-    //     .then(weatherData => {
-    //         // @ts-ignore
-    //         const dataToDisplay = weatherData.main.map(data => {
-    //             return {
-    //                 temp: data.temp,
-    //                 feelsLike: data.feels_like,
-    //                 pressure: data.pressure,
-    //                 humidity: data.humidity
-    //             }
-    //         })
-    }
+    function fetchOpenWeather() {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${OPEN_WEATHER_KEY}`)
+        .then(response => {
+            console.log(response)
+            return response.json()
+        })
+        .then(weatherData => {
+            // @ts-ignore
+            const dataToDisplay = weatherData.main.map(data => {
+                return {
+                    temp: data.temp,
+                    feelsLike: data.feels_like,
+                    pressure: data.pressure,
+                    humidity: data.humidity
+                }
+            })
+        })
+        }
 
     return (
-        <div className="form-container">
-            <form>
-                <input type="number" min="-180.0000" max="180.0000" step="0.0001" placeholder="longitude" onChange={LON_HANDLER} />
-                <input type="number" min="-90.0000" max="90.0000" step="0.0001" placeholder="latitude" onChange={LAT_HANDLER} />
-            </form>
-            <div className="providers-container">
-                <div className="img-container">
-                <button onClick={FETCH_OPEN_WEATHER}>good weather</button>
+            <div className="form-container">
+                <form>
+                    <input type="number" min="-180.0000" max="180.0000" step="0.0001" placeholder="longitude" onChange={LON_HANDLER} />
+                    <input type="number" min="-90.0000" max="90.0000" step="0.0001" placeholder="latitude" onChange={LAT_HANDLER} />
+                </form>
+                <div className="providers-container">
+                    <div className="img-container">
+                        <button onClick={fetchOpenWeather}>good weather</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
