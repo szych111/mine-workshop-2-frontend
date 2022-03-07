039861f41d11c799385f5d7d5fd4d7696b66bb26
Alex <szychlinska@gmail.com>
2022-03-07 22:36:49 +0100

    OW and VC fetch

--- src/Form.tsx
+++ src/Form.tsx
@@ -6,7 +6,15 @@ import './index.css';
 export const Form: FC = () => {
     const [LAT, setLat] = useState(52.2297)
     const [LON, setLon] = useState(21.0122)
-    const [WEATHER_DATA, setWEATHER_DATA] = useState({})
+    const [WEATHER_DATA, setWEATHER_DATA] = useState<{
+        provider?: string,
+        place?: string,
+        country?: string,
+        temperature?: number,
+        feelsLike?: number,
+        pressure?: number,
+        windSpeed?: number,
+    }>({})
 
 
     const LON_HANDLER = (e: any) => {
@@ -17,85 +25,90 @@ export const Form: FC = () => {
         setLat(e.target.value)
     }
 
-    const OPEN_WEATHER_KEY = "16ce5520bd032264b1ac7375550e162d"
-    const VISUAL_CROSSING_KEY = "733QT6TG43YBN3HJCYTAQEQTY"
-
     async function fetchOpenWeather() {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${OPEN_WEATHER_KEY}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`)
         const data = await response.json()
         console.log(data)
-        
-        setWEATHER_DATA(data)
+
+        setWEATHER_DATA({
+            temperature: data.main.temp,
+            feelsLike: data.main.feels_like,
+            pressure: data.main.pressure,
+            windSpeed: data.wind.speed,
+            place: data.name,
+            country: data.sys.country,
+            provider: 'OpenWeather'
+        })
         console.log(WEATHER_DATA)
     }
 
-    // function fetchVisualCrossing() {
-    //     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${LAT},${LON}?key=${VISUAL_CROSSING_KEY}`)
-    //         .then(response => {
-    //             console.log(response)
-    //             return response.json()
-    //         })
-    //         .then(weatherData => {
-    //             // @ts-ignore
-    //             const dataToDisplay = weatherData.main.map(data => {
-    //                 return {
-    //                     temp: data.temp,
-    //                     feelsLike: data.feels_like,
-    //                     pressure: data.pressure,
-    //                     humidity: data.humidity
-    //                 }
-    //             })
-    //         })
-    // }
+
+    async function fetchVisualCrossing() {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${LAT},${LON}?key=${process.env.REACT_APP_VISUAL_CROSSING_KEY}`)
+        // const data = await response.json()
+        console.log(response)
+    }
 
     // elements=temp,feelslike,pressure,humidity
 
     return (
         <main>
             <div className="form-container">
-            <form>
-                <input type="number" min="-90.0000" max="90.0000" step="0.0001" placeholder="latitude" onChange={LAT_HANDLER} />
-                <input type="number" min="-180.0000" max="180.0000" step="0.0001" placeholder="longitude" onChange={LON_HANDLER} />
-            </form>
-            <div className="providers-container">
-                <div className="img-container">
-                    <button onClick={fetchOpenWeather}>OpenWeather</button>
-                    <button >VisualCrossing</button>
+                <form>
+                    <input type="number" min="-90.0000" max="90.0000" step="0.0001" placeholder="latitude" onChange={LAT_HANDLER} />
+                    <input type="number" min="-180.0000" max="180.0000" step="0.0001" placeholder="longitude" onChange={LON_HANDLER} />
+                </form>
+                <div className="providers-container">
+                    <div className="img-container">
+                        <button onClick={fetchOpenWeather}>OpenWeather</button>
+                        <button onClick={fetchVisualCrossing} >VisualCrossing</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        {/* <div className='data-container'>
-            <div className='line'>
-                <p className='description'>Temperature</p>
-                <p className='unit'>
-                    <span className='temp-number'>{WEATHER_DATA.temperature}</span>
-                    <span className='temp-degrees'>o</span>
-                    <span className='temp-scale'></span>
-                </p>
+            <div className='data-container'>
+                <div className='line'>
+                    <p className='description'>Weather Data Provider</p>
+                    <p className='unit'>
+                        <span className='temp-number'>{WEATHER_DATA.provider}</span>
+                    </p>
+                </div>
+                <div className='line'>
+                    <p className='description'>Place</p>
+                    <p className='unit'>
+                        <span className='temp-number'>{WEATHER_DATA.place}, {WEATHER_DATA.country}</span>
+                    </p>
+                </div>
+                <div className='line'>
+                    <p className='description'>Temperature</p>
+                    <p className='unit'>
+                        <span className='temp-number'>{WEATHER_DATA.temperature}</span>
+                        <span className='temp-degrees'>o</span>
+                        <span className='temp-scale'>F</span>
+                    </p>
+                </div>
+                <div className='line'>
+                    <p className='description'>Wind speed</p>
+                    <p className='unit'>
+                        <span className='wind-number'>{WEATHER_DATA.feelsLike}</span>
+                        <span className='wind-unit'></span>
+                        <span className='wind-hour'>/ hour</span>
+                    </p>
+                </div>
+                <div className='line'>
+                    <p className='description'>Pressure</p>
+                    <p className='unit'>
+                        <span className='pressure-number'>{WEATHER_DATA.pressure}</span>
+                        <span className='pressure-unit'></span>
+                    </p>
+                </div>
+                <div className='line'>
+                    <p className='description'>Humidity</p>
+                    <p className='unit'>
+                        <span className='pressure-number'>{WEATHER_DATA.windSpeed}</span>
+                        <span className='pressure-unit'></span>
+                    </p>
+                </div>
             </div>
-            <div className='line'>
-                <p className='description'>Wind speed</p>
-                <p className='unit'>
-                    <span className='wind-number'>{feelsLike}</span>
-                    <span className='wind-unit'></span>
-                    <span className='wind-hour'>/ hour</span>
-                </p>
-            </div>
-            <div className='line'>
-                <p className='description'>Pressure</p>
-                <p className='unit'>
-                    <span className='pressure-number'>{pressure}</span>
-                    <span className='pressure-unit'></span>
-                </p>
-            </div>
-            <div className='line'>
-                <p className='description'>Humidity</p>
-                <p className='unit'>
-                    <span className='pressure-number'>{humidity}</span>
-                    <span className='pressure-unit'></span>
-                </p>
-            </div>
-        </div> */}
         </main>
     )
 }
